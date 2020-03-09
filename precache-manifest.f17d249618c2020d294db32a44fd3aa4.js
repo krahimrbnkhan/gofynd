052bb333ef4efcfbffe4ded8b7a6ef37ae0ab66c
Rahim Khan <rahim.k@turtlemint.com>
2020-03-09 12:09:16 +0530

    Updates

---
+++ precache-manifest.f17d249618c2020d294db32a44fd3aa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf7fc8754cf3bd46f5e34a03c1f806bf",
+    "url": "/gofynd/index.html"
+  },
+  {
+    "revision": "1ff762f188bf94d54cbd",
+    "url": "/gofynd/static/css/main.122168b5.chunk.css"
+  },
+  {
+    "revision": "3f0a746c050622f06b69",
+    "url": "/gofynd/static/js/2.655ddf1c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/gofynd/static/js/2.655ddf1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ff762f188bf94d54cbd",
+    "url": "/gofynd/static/js/main.282176f9.chunk.js"
+  },
+  {
+    "revision": "b8bc646e95a3abb92bcd",
+    "url": "/gofynd/static/js/runtime-main.0427f08c.js"
+  }
+]);
